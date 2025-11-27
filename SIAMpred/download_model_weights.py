c7fa312d78008d6f1d06c65bfa660626731b386d
valabregue <romain.valabregue@upmc.fr>
2025-11-27 08:35:43 +0100

    python function just to download weights

---
+++ SIAMpred/download_model_weights.py
@@ -0,0 +1,7 @@
+from SIAMpred.nn_prediction import get_model_path_and_fold
+
+if __name__ == "__main__":
+    """
+    Download all pretrained weights default num 0 model
+    """
+    get_model_path_and_fold(num_model=0 )
