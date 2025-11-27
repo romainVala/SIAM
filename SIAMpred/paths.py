5624c9bcba1dbe9a106e3085382d10b0c3b6201f
valabregue <romain.valabregue@upmc.fr>
2025-11-27 08:37:22 +0100

    clean local path for nnunet model dir

--- SIAMpred/paths.py
+++ SIAMpred/paths.py
@@ -55,168 +55,163 @@ def get_siam_model_dir():
             print(f"model parameter from SIAM_MODEL_DIR env variable {model_dir}")
         else:
             raise ValueError(f'Error the environement variable SIAM_MODEL_DIR point to a no existing dir: {model_dir}')
+    elif 'nnUNet_results' in os.environ: #just an other (natural option to quickly test fresh trained model)
+        model_dir = os.environ.get('nnUNet_results')
+
     else:
         model_dir = os.path.join(os.path.expanduser('~'), 'siam_params')
 
     return model_dir
 
 def get_model_path_and_fold(num_model:int ):
-    if num_model==-1: #get it from zenodo
+    siam_model_dir = get_siam_model_dir()
+
+    if num_model==0: #get it from zenodo
         # the link where to get zip version of the models
         ZENODO_DOWNLOAD_URL = 'https://zenodo.org/records/15055596/files/model_708.zip?download=1'
-        siam_model_dir = get_siam_model_dir()
         res_folder = os.path.join(siam_model_dir, 'v0.1')
 
         out_prefix = 'siamV01_'
         maybe_download_parameters(res_folder, ZENODO_DOWNLOAD_URL)
 
-    elif num_model==0: #get it from zenodo
+    elif num_model==2: #get it from zenodo
         # the link where to get zip version of the models
         ZENODO_DOWNLOAD_URL = 'https://zenodo.org/records/15780983/files/DS715_NODA.tar.gz?download=1' # https://doi.org/10.5281/zenodo.15780983
-
-        siam_model_dir = get_siam_model_dir()
         res_folder = os.path.join(siam_model_dir, 'v0.2','DS715_NODA')
 
         out_prefix = 'siamV02_'
         maybe_download_parameters_tarfile(res_folder, ZENODO_DOWNLOAD_URL)
+    elif num_model==1: #get it from zenodo
+        # the link where to get zip version of the models
+        ZENODO_DOWNLOAD_URL = 'https://zenodo.org/records/15055596/files/model_708.zip?download=1'
+        res_folder = os.path.join(siam_model_dir, 'v0.1')
+
+        out_prefix = 'siamV01_'
+        maybe_download_parameters(res_folder, ZENODO_DOWNLOAD_URL)
 
     else:
 
-        nnres_path = os.environ.get('nnUNet_results')
-        if nnres_path is None:
-            print(f'you choose num_model {num_model} For local modem (-m num  (with num>0) you need to specify the environement varaible  nnUNet_results')
-            raise('rrr')
-
         if num_model == 111:
-            res_folder = os.path.join(nnres_path, 'Dataset710_Vasc2suj_v3_Region',
+            res_folder = os.path.join(siam_model_dir, 'Dataset710_Vasc2suj_v3_Region',
                                       'nnUNetTrainer__nnUNetResEncUNetLPlans__3d_fullres')
             out_prefix = 'pred_DS710_3ResEncL_'
 
         elif num_model == 1:
-            res_folder = os.path.join(nnres_path, 'Dataset710_Vasc2suj_v3_Region',
+            res_folder = os.path.join(siam_model_dir, 'Dataset710_Vasc2suj_v3_Region',
                                       'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS710_5ResEncXL_'
 
         elif num_model == 102:
-            res_folder = os.path.join(nnres_path, 'Dataset710_Vasc2suj_v3_Region',
+            res_folder = os.path.join(siam_model_dir, 'Dataset710_Vasc2suj_v3_Region',
                                       'nnUNetTrainer__nnUNetPlans_50G__3d_fullres')
             out_prefix = 'pred_DS710_5nn2Pass_'
 
         elif num_model == 2:
             #img 450^3 18 mn GPU A100 80G 24 cpu
-            res_folder = os.path.join(nnres_path, 'Dataset709_Vasc2suj_v3',
+            res_folder = os.path.join(siam_model_dir, 'Dataset709_Vasc2suj_v3',
                                       'nnUNetTrainer__nnUNetPlans_50G__3d_fullres_first_pass')
             out_prefix = 'pred_DS709_3nnP50G_'
 
         elif num_model == 9:
             #img 450^3 18 mn GPU A100 80G 24 cpu
-            res_folder = [os.path.join(nnres_path, 'Dataset709_Vasc2suj_v3',
+            res_folder = [os.path.join(siam_model_dir, 'Dataset709_Vasc2suj_v3',
                                        'nnUNetTrainer_onlyMirror01__nnUNetResEncUNetLPlans__3d_lowres'),
-                          os.path.join(nnres_path, 'Dataset709_Vasc2suj_v3',
+                          os.path.join(siam_model_dir, 'Dataset709_Vasc2suj_v3',
                                       'nnUNetTrainerNoDA__nnUNetResEncUNetLPlans__3d_cascade_fullres') ]
             out_prefix = 'pred_DS709_CascadeNoDA_'
 
         elif num_model == 3:
             #img 450^3 18 mn GPU A100 80G 24 cpu
-            res_folder = os.path.join(nnres_path, 'Dataset708_Ultra_SkulVasc40',
+            res_folder = os.path.join(siam_model_dir, 'Dataset708_Ultra_SkulVasc40',
                                       'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS708_5nnResXL_'
 
         elif num_model == 4:
             #img 450^3 18 mn GPU A100 80G 24 cpu
-            res_folder = os.path.join(nnres_path, 'Dataset706_Vasc2suj_l22_v1',
+            res_folder = os.path.join(siam_model_dir, 'Dataset706_Vasc2suj_l22_v1',
                                       'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS706_5nnResXL_'
 
         elif num_model == 5:
             #img 450^3 18 mn GPU A100 80G 24 cpu
-            res_folder = os.path.join(nnres_path, 'Dataset704_Uhcp_skv52_mot_elaBig',
+            res_folder = os.path.join(siam_model_dir, 'Dataset704_Uhcp_skv52_mot_elaBig',
                                       'nnUNetTrainer__nnUNetPlannerResEncXL_80G__3d_fullres')
             out_prefix = 'pred_DS704_3nnResXL_'
 
         elif num_model ==8:
-            res_folder = os.path.join(nnres_path, 'Dataset718_Ultra_SkulVasc40_TumorBrast', 'nnUNetTrainer__nnUNetResEncUNetLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset718_Ultra_SkulVasc40_TumorBrast', 'nnUNetTrainer__nnUNetResEncUNetLPlans__3d_fullres')
             out_prefix = 'pred_DS718_5nnResL'
 
         elif num_model ==88:
-            res_folder = os.path.join(nnres_path, 'Dataset718_Ultra_SkulVasc40_TumorBrast', 'nnUNetTrainer__nnUNetResEncUNetXXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset718_Ultra_SkulVasc40_TumorBrast', 'nnUNetTrainer__nnUNetResEncUNetXXLPlans__3d_fullres')
             out_prefix = 'pred_DS718_5nnResXXL'
 
         elif num_model ==12:
-            res_folder = os.path.join(nnres_path, 'Dataset712_Vasc2suj_v3_Few', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset712_Vasc2suj_v3_Few', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS712_5ResXL'
 
         elif num_model ==122:
-            res_folder = os.path.join(nnres_path, 'Dataset712_Vasc2suj_v3_Few', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset712_Vasc2suj_v3_Few', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS712_NODA3ResXL'
 
         elif num_model ==1212:
-            res_folder = os.path.join(nnres_path, 'Dataset712_Vasc2suj_v3_Few', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset712_Vasc2suj_v3_Few', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS712_SubR_'
-            sub_region = gdir(nnres_path,['Dataset712[0123456789]','nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans'])
+            sub_region = gdir(siam_model_dir,['Dataset712[0123456789]','nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans'])
             res_folder = [res_folder, sub_region]
 
         elif num_model ==14:
-            res_folder = os.path.join(nnres_path, 'Dataset714_MidaSuj3_RFew', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset714_MidaSuj3_RFew', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS714_5ResXL'
 
         elif num_model ==1414:
-            res_folder = os.path.join(nnres_path, 'Dataset714_MidaSuj3_RFew', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset714_MidaSuj3_RFew', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS714_SubR_'
-            sub_region = gdir(nnres_path,['Dataset714[0123456789]','nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans'])
+            sub_region = gdir(siam_model_dir,['Dataset714[0123456789]','nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans'])
             res_folder = [res_folder, sub_region]
 
         elif num_model ==13:
-            res_folder = os.path.join(nnres_path, 'Dataset713_MidaSuj1', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset713_MidaSuj1', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS713_3ResXL'
 
         elif num_model ==15:
-            res_folder = os.path.join(nnres_path, 'Dataset715_MixSuj6', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset715_MixSuj6', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS715_NODA3ResXL'
         elif num_model ==152:#do not exist (bad Skull)
-            res_folder = os.path.join(nnres_path, 'Dataset715_MixSuj6', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset715_MixSuj6', 'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS715_3ResXL'
         elif num_model ==16:
-            res_folder = os.path.join(nnres_path, 'Dataset716_MixLowDill', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset716_MixLowDill', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS716_NODA'
         elif num_model ==17:
-            res_folder = os.path.join(nnres_path, 'Dataset717_MixLowDill_Ano', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset717_MixLowDill_Ano', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS717_Ano'
             
         elif num_model == 101:
             # XXX ines tumor + ms + 716 train
-            ines_nnunet_res = "/network/iss/cenir/analyse/irm/users/ines.khemir/nnunet/Results/"
-            res_folder = os.path.join(ines_nnunet_res, 'Dataset1001_ms_tumor_MixLowDill', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset1001_ms_tumor_MixLowDill', 'nnUNetTrainerNoDA__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS101_ms_tumor_MixLowDill'
         elif num_model == 102:
             # XXX ines (tumor + ms) without contrast constraint + 716 train : probleme plan normale pas XL
-            ines_nnunet_res = "/network/iss/cenir/analyse/irm/users/ines.khemir/nnunet/Results/"
-            res_folder = os.path.join(ines_nnunet_res, 'Dataset1002_ms_tumor_WC_MixLowDill', 'nnUNetTrainer__nnUNetPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset1002_ms_tumor_WC_MixLowDill', 'nnUNetTrainer__nnUNetPlans__3d_fullres')
             out_prefix = "pred_DS102_ms_tumor_MixLowDill"
-        elif num_model == 104:
-            # XXX ines ms only without constraint : probleme plan normale pas XL
-            ines_nnunet_res = "/network/iss/cenir/analyse/irm/users/ines.khemir/nnunet/Results/"
-            res_folder = os.path.join(ines_nnunet_res, 'Dataset1004_ms_MixLowDill', 'nnUNetTrainer__nnUNetPlans__3d_fullres')
-            out_prefix = "pred_DS104_ms_MixLowDill"
         elif num_model == 106:
             # XXX ines healthy with GM and cerb CSF push
-            ines_nnunet_res = "/network/iss/cenir/analyse/irm/users/ines.khemir/nnunet/Results/"
-            res_folder = os.path.join(ines_nnunet_res, 'Dataset1006_Partialization_MixLowDill',
+            res_folder = os.path.join(siam_model_dir, 'Dataset1006_Partialization_MixLowDill',
                                       'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = "pred_DS106_MixLow_csfPush"
         elif num_model == 107:
             # XXX ines 33% healthy with GM and cerb CSF push, 33% ms and 33% of tumor
-            ines_nnunet_res = "/network/iss/cenir/analyse/irm/users/ines.khemir/nnunet/Results/"
-            res_folder = os.path.join(ines_nnunet_res, 'Dataset1007_Partialization_MixLowDill',
+            res_folder = os.path.join(siam_model_dir, 'Dataset1007_Partialization_MixLowDill',
                                       'nnUNetTrainer__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = "pred_DS107_MixLow_csfPush_ms_tumor"
         elif num_model == 108:
             # same as 107 ep4000
-            res_folder = os.path.join(nnres_path, 'Dataset1007_Partialization_MixLowDill', 'nnUNetTrainerNoDA4000__nnUNetResEncUNetXLPlans__3d_fullres')
+            res_folder = os.path.join(siam_model_dir, 'Dataset1007_Partialization_MixLowDill', 'nnUNetTrainerNoDA4000__nnUNetResEncUNetXLPlans__3d_fullres')
             out_prefix = 'pred_DS108_LcsfP_Ano'
         elif num_model == 109:
             # same as 107 ep4000
-            res_folder = os.path.join(nnres_path, 'Dataset1007_Partialization_MixLowDill', 'nnUNetTrainerNoDA4000__nnUNetResEncUNetXLPlans__3d_fullres_average')
+            res_folder = os.path.join(siam_model_dir, 'Dataset1007_Partialization_MixLowDill', 'nnUNetTrainerNoDA4000__nnUNetResEncUNetXLPlans__3d_fullres_average')
             out_prefix = 'pred_DS109_LcsfP_Ano'
 
 
