741d16abd0676c10dc18e4fbd8998bd00a535de0
valabregue <romain.valabregue@upmc.fr>
2026-06-08 16:02:17 +0200

    add option to select model name

--- SIAMpred/download_model_weights.py
+++ SIAMpred/download_model_weights.py
@@ -6,4 +6,12 @@ if __name__ == "__main__":
     """
     Download all pretrained weights default num 0 model
     """
-    get_model_path_and_fold(num_model=0 )
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-m', '--model', help='Optional: For local use only : An integer to specify which model '
+                                              'use -1 to have version 0.1 model '
+                        ,default=0, type=int, required=False)
+    args = parser.parse_args()
+
+    get_model_path_and_fold(num_model=args.model )
